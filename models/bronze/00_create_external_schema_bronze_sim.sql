21d4d43a5c6f215f26d492be739015d4c150cd1e
Moussa Sow Dieme <lermitejiraya05@gmail.com>
2025-12-14 23:58:52 +0000

    fix(sql): correction du script de création du schéma externe 'bronze_sim'
    - Remplacement du nom de base Glue invalide (tiret → underscore)
    - Ajustement de la commande pour compatibilité Redshift Serverless

--- models/bronze/00_create_external_schema_bronze_sim.sql
+++ models/bronze/00_create_external_schema_bronze_sim.sql
@@ -1,5 +1,5 @@
 CREATE EXTERNAL SCHEMA bronze_sim
 FROM DATA CATALOG
-DATABASE 'olist_dw-sim-catalog'
+DATABASE 'olist_dw_sim_catalog'
 IAM_ROLE 'arn:aws:iam::xxxxxxxxxxxxxxxxxxxx'
 CREATE EXTERNAL DATABASE IF NOT EXISTS;
