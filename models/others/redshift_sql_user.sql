756390d3a65ed2832bf4502a954097dfbfec8251
Moussa Sow Dieme <lermitejiraya05@gmail.com>
2025-12-15 16:33:52 +0000

    fix(redshift-admin): mise à jour du script de création de l’utilisateur bi_analyst - suppression de la commande GRANT CONNECT non supportée par Redshift Serverless - ajout des privilèges USAGE sur le schéma externe olist_spectrum_schema - ajout des privilèges SELECT sur toutes les tables du schéma externe - configuration des privilèges par défaut pour garantir l’accès aux futures tables
     Changes to be committed:
    	modified:   models/others/redshift_sql_user.sql

--- models/others/redshift_sql_user.sql
+++ models/others/redshift_sql_user.sql
@@ -6,5 +6,9 @@ GRANT CONNECT ON DATABASE dev TO bi_analyst;
 -- Autoriser l'utilisateur à voir le schéma externe créé
 GRANT USAGE ON SCHEMA olist_spectrum_schema TO bi_analyst;
 
--- Autoriser la sélection sur la table Parquet
-GRANT SELECT ON olist_spectrum_schema.table_order_sim TO bi_analyst;
+
+-- Donner accès en lecture seule à la table
+GRANT SELECT ON ALL TABLES IN SCHEMA olist_spectrum_schema TO bi_analyst;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA olist_spectrum_schema
+GRANT SELECT ON TABLES TO bi_analyst;
