a963c938f11fcf288429ebb593efafc3630b344e
Moussa Sow Dieme <lermitejiraya05@gmail.com>
2025-12-17 10:05:04 +0000

    git commit -m "fix(silver): résolution erreur Spectrum nested query et optimisation incrémentale Contexte : Le modèle silver_sim échouait avec une erreur 'Spectrum nested query error' lors de la manipulation de types SUPER combinée à des filtres incrémentaux.
    Modifications apportées :
    - Implémentation d'une macro de pré-exécution Jinja pour capturer le MAX(order_purchase_timestamp) avant le bloc principal.
    - Utilisation du contrôle des blancs Jinja (dash notation) pour éviter les erreurs de syntaxe SQL sur Redshift.
    - Sécurisation de la variable 'max_date' avec le flag 'if execute' pour la phase de compilation dbt.
    
    Impact :
    - Correction du bug bloquant sur la couche Silver.
    - Succès de la chaîne de dépendance (Silver -> Gold) validé en 25.66s.
    - Réduction confirmée du volume de données scannées lors des futurs runs."
    - utilisation de dbt run --full-refresh pour reset toute la table cette fois seulement.
     Changes to be committed:
    	modified:   models/silver/silver_sim.sql

--- models/silver/silver_sim.sql
+++ models/silver/silver_sim.sql
@@ -3,6 +3,17 @@
     unique_key='order_id'
 ) }}
 
+{%- set max_date = None -%}
+{%- if is_incremental() -%}
+    {%- set max_date_query -%}
+        SELECT MAX(order_purchase_timestamp) FROM {{ this }}
+    {%- endset -%}
+    {%- set results = run_query(max_date_query) -%}
+    {%- if execute -%}
+        {%- set max_date = results.columns[0][0] -%}
+    {%- endif -%}
+{%- endif -%}
+
 WITH unpivot_items AS (
     SELECT
         t.order_id,
@@ -17,11 +28,9 @@ WITH unpivot_items AS (
         t.items AS item
     WHERE
         t.order_status = 'approved'
-        
-    {% if is_incremental() %}
-      -- Filtre : On ne prend que ce qui est plus récent que la date max déjà en table
-      AND t.order_purchase_timestamp > (select max(order_purchase_timestamp) from {{ this }})
-    {% endif %}
+        {% if is_incremental() and max_date is not none %}
+        AND t.order_purchase_timestamp > '{{ max_date }}'
+        {% endif %}
 )
 
 SELECT
